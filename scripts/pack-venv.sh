1706d368018564a5b2b2a1c12e11ddf9846b428b
hinnytsang <mhtsangab@connect.ust.hk>
2023-03-06 15:45:43 +0800

    [dev] Update script to pack the requirements

--- scripts/pack-venv.sh
+++ scripts/pack-venv.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Export the dependencies to a requirements.txt file
-poetry export -f requirements.txt --output requirements.txt
-
 # Create a new virtual environment and install the dependencies:
 python -m venv venv
 source venv/bin/activate
